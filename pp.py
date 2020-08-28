2bdf83ffd9fb8747168659d4bed81b90cfb96fab
tetsushiwahaha <ueta@tokushima-u.ac.jp>
2020-08-28 09:31:53 +0900

    corrections done

--- pp.py
+++ pp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # Autonomous ODE simulator. 
-# Special Thank to S. Amoh on 2020/05/09
 #
 
 import sys, json
@@ -17,18 +16,18 @@ def func(t, x, data):
         v.append(eval(data.dict['func'][k]))
     return v
 
-def poincare(t, x, data):
-	return x[data.dict['p_index']] - data.dict['p_position']
+def poincare_section(t, x, data):
+	return x[data.dict['p_index']] - data.dict['p_location']
 
 
 def main():
 	data = pptools.init()
 	state0 = data.dict['x0']
 	tick = data.dict['tick']
-	duration = 1.0
+	duration = 0.5
 	period = 0.0
-	poincare.direction = -1
-	poincare.terminal = True
+	poincare_section.direction = -1
+	poincare_section.terminal = True
 	running = True	
 	
 	while running:
@@ -38,7 +37,7 @@ def main():
 			#method = 'DOP853', 
 			method = 'RK45', 
 			args = (data,), 
-			events = poincare, max_step = tick,
+			events = poincare_section, max_step = tick,
 			rtol = 1e-12, 
 			dense_output = True
 			)
@@ -50,17 +49,17 @@ def main():
 		if state.status == 1:	# A termination event occurred
 			data.dict['x0'] = s = state.y_events[0][-1]
 			plt.plot(s[data.dispx], s[data.dispy], 'o', 
-				markersize = 2, color="red",
-				 alpha = data.dict['alpha'])
+				markersize = 2, color="red", alpha = data.dict['alpha'])
 			period += state.t_events[0][-1]
 			#print(period)
 			data.dict['period'] = period
-			duration = period/2 + 0.1 # make a new duration appropriately
+			if period > duration:
+				duration = period * 0.51
 			period = 0.0
-			poincare.terminal = False
+			poincare_section.terminal = False
 		else:
-			poincare.terminal = True
 			period += duration
+			poincare_section.terminal = True
 		state0 = data.now = state.y[:, -1] # data.now: clicked point
 		plt.pause(0.001) #REQIRED
 
